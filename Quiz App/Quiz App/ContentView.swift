326165b72343e61cceccc833411e936c88cda062
MihalevD <91677718+MihalevD@users.noreply.github.com>
2022-02-21 02:44:37 +0200

    init Quiz App commit with Pods

---
+++ Quiz App/Quiz App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz App
+//
+//  Created by Denislav's Mac Book on 21.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz App/Quiz App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz App
+//
+//  Created by Denislav's Mac Book on 21.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
